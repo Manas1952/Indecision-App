80ce233c81be3b6e38577249cf7674dc9a71cb27
Manas1952 <purohitmanas36@gmail.com>
2022-02-02 12:50:55 +0530

    deployed using server.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CSSExtract = new ExtractTextPlugin('styles.css')
 
 module.exports = {
   entry: './src/app.js',
@@ -13,13 +15,27 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
+      use: CSSExtract.extract({
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true // will give sourceMappingURL(you can see by selecting any element in devtool and see it's styles, it would show you something like abc.css)
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      })
     }]
   },
+  plugins: [
+    CSSExtract
+  ],
   devtool: 'eval-cheap-module-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public')
